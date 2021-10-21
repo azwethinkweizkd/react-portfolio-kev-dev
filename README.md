b4bf8575b62df1a28569c9feb7bd91107d256ffc
Kevin Devlin <azwethinkweizkd@gmail.com>
2021-10-20 21:26:32 -0600

    Moved project info to a JSON file to map through on portfolio page

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# react-portfolio-kev-dev
+# React Portfolio Kevin Devlin [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+## Description
+
+Displaying my react skills with a simple personal site for my Resume and Portfolio of several projects completed through the duration of the Denver University - Full Stack Web Development bootcamp.
+
+## Table of Contents
+
+- ### [Installation](#installation)
+
+- ### [Usage](#usage)
+
+- ### [Contribution](#contribution)
+
+- ### [Test Instructions](#testInstructions)
+
+- ### [License](#License)
+
+## Installation
+
+No installation is required for this project and is stickly for the use of the author to highlight and display their skills in web developmemt.
+
+## Usage
+
+Interested in a future employee that has MERN Full Stack Web Development experience? This site, designed and developed by Kevin Devlin, showcases many skills in React, JS, Node, APIs, and Database Development.
+
+## Contribution
+
+Kevin Devlin - Author
+
+GitHub Username:
+azwethinkweizkd
+
+## Test Instructions
+
+There are no test instructions currenly implemented for this site.
+
+## License
+
+    This project was created using MIT
--- src/pages/portfolioBrick.js
+++ src/pages/portfolioBrick.js
@@ -7,7 +7,9 @@ import Masonry from "@mui/lab/Masonry";
 import MasonryItem from "@mui/lab/MasonryItem";
 import { Typography } from "@mui/material";
 import ButtonBase from "@mui/material/ButtonBase";
+import projData from "../utils/projInfo.json";
 
+// console.log(projData);
 const ImageButton = styled(ButtonBase)(({ theme }) => ({
   position: "relative",
   height: 200,
@@ -71,286 +73,46 @@ const ImageMarked = styled("span")(({ theme }) => ({
   left: "calc(50% - 9px)",
   transition: theme.transitions.create("opacity"),
 }));
+
 export default function MasonPortfolio() {
   return (
     <Box sx={{ width: "auto", minHeight: 829 }}>
       <Masonry columns={3} spacing={1}>
-        <MasonryItem
-          key={
-            process.env.PUBLIC_URL +
-            "/images/thimo-pedersen-TWCnHKKhqSo-unsplash.jpg"
-          }
-          columnSpan={2}
-        >
-          <ImageButton
-            focusRipple
-            key="Choose your pokemon"
-            href="https://github.com/bonilhav/Choose_Your_Pokemon"
-            rel="noopener"
-          >
-            <ImageSrc
-              style={{
-                backgroundImage: `url(${
-                  process.env.PUBLIC_URL +
-                  "/images/thimo-pedersen-TWCnHKKhqSo-unsplash.jpg"
-                })`,
-              }}
-            />
-            <ImageBackdrop className="MuiImageBackdrop-root" />
-            <Image>
-              <Typography
-                component="span"
-                variant="subtitle1"
-                color="inherit"
-                sx={{
-                  position: "relative",
-                  p: 4,
-                  pt: 2,
-                  pb: (theme) => `calc(${theme.spacing(1)} + 6px)`,
+        {projData.map((proj, index) => (
+          <MasonryItem key={index} columnSpan={proj.col}>
+            <ImageButton
+              focusRipple
+              key={proj.title}
+              href={proj.href}
+              rel="noopener"
+            >
+              <ImageSrc
+                style={{
+                  backgroundImage: `url(${
+                    process.env.PUBLIC_URL + proj.image
+                  })`,
                 }}
-              >
-                Choose your pokemon
-                <ImageMarked className="MuiImageMarked-root" />
-              </Typography>
-            </Image>
-          </ImageButton>
-        </MasonryItem>
-        <MasonryItem
-          key={
-            process.env.PUBLIC_URL +
-            "/images/samuel-girven-VJ2s0c20qCo-unsplash.jpg"
-          }
-        >
-          <ImageButton
-            focusRipple
-            key="Fitness Tracker"
-            href="https://sim-and-sin-fit-track.herokuapp.com/"
-            rel="noopener"
-          >
-            <ImageSrc
-              style={{
-                backgroundImage: `url(${
-                  process.env.PUBLIC_URL +
-                  "/images/samuel-girven-VJ2s0c20qCo-unsplash.jpg"
-                })`,
-              }}
-            />
-            <ImageBackdrop className="MuiImageBackdrop-root" />
-            <Image>
-              <Typography
-                component="span"
-                variant="subtitle1"
-                color="inherit"
-                sx={{
-                  position: "relative",
-                  p: 4,
-                  pt: 2,
-                  pb: (theme) => `calc(${theme.spacing(1)} + 6px)`,
-                }}
-              >
-                Simple & Sinister Fitness Tracker
-                <ImageMarked className="MuiImageMarked-root" />
-              </Typography>
-            </Image>
-          </ImageButton>
-        </MasonryItem>
-        <MasonryItem
-          key={
-            process.env.PUBLIC_URL +
-            "/images/christin-hume-Hcfwew744z4-unsplash.jpg"
-          }
-        >
-          <ImageButton
-            focusRipple
-            key="Fitness Tracker"
-            href="https://code-tech-chit-chat.herokuapp.com/"
-            rel="noopener"
-          >
-            <ImageSrc
-              style={{
-                backgroundImage: `url(${
-                  process.env.PUBLIC_URL +
-                  "/images/christin-hume-Hcfwew744z4-unsplash.jpg"
-                })`,
-              }}
-            />
-            <ImageBackdrop className="MuiImageBackdrop-root" />
-            <Image>
-              <Typography
-                component="span"
-                variant="subtitle1"
-                color="inherit"
-                sx={{
-                  position: "relative",
-                  p: 4,
-                  pt: 2,
-                  pb: (theme) => `calc(${theme.spacing(1)} + 6px)`,
-                }}
-              >
-                CodeTechChitChat
-                <ImageMarked className="MuiImageMarked-root" />
-              </Typography>
-            </Image>
-          </ImageButton>
-        </MasonryItem>
-        <MasonryItem
-          key={
-            process.env.PUBLIC_URL +
-            "/images/katie-smith-uQs1802D0CQ-unsplash.jpg"
-          }
-          columnSpan={2}
-        >
-          <ImageButton
-            focusRipple
-            key="Fitness Tracker"
-            href="https://kitchenforager.herokuapp.com/"
-            rel="noopener"
-          >
-            <ImageSrc
-              style={{
-                backgroundImage: `url(${
-                  process.env.PUBLIC_URL +
-                  "/images/katie-smith-uQs1802D0CQ-unsplash.jpg"
-                })`,
-              }}
-            />
-            <ImageBackdrop className="MuiImageBackdrop-root" />
-            <Image>
-              <Typography
-                component="span"
-                variant="subtitle1"
-                color="inherit"
-                sx={{
-                  position: "relative",
-                  p: 4,
-                  pt: 2,
-                  pb: (theme) => `calc(${theme.spacing(1)} + 6px)`,
-                }}
-              >
-                Kitchen Forager
-                <ImageMarked className="MuiImageMarked-root" />
-              </Typography>
-            </Image>
-          </ImageButton>
-        </MasonryItem>
-        <MasonryItem
-          key={
-            process.env.PUBLIC_URL +
-            "/images/vincent-ledvina-a1SAq0v6AyY-unsplash.jpg"
-          }
-          columnSpan={2}
-        >
-          <ImageButton
-            focusRipple
-            key="14er Quiz"
-            href="https://azwethinkweizkd.github.io/High-Altitude-CO-14er-Quiz/"
-            rel="noopener"
-          >
-            <ImageSrc
-              style={{
-                backgroundImage: `url(${
-                  process.env.PUBLIC_URL +
-                  "/images/vincent-ledvina-a1SAq0v6AyY-unsplash.jpg"
-                })`,
-              }}
-            />
-            <ImageBackdrop className="MuiImageBackdrop-root" />
-            <Image>
-              <Typography
-                component="span"
-                variant="subtitle1"
-                color="inherit"
-                sx={{
-                  position: "relative",
-                  p: 4,
-                  pt: 2,
-                  pb: (theme) => `calc(${theme.spacing(1)} + 6px)`,
-                }}
-              >
-                Colorado 14er Quiz
-                <ImageMarked className="MuiImageMarked-root" />
-              </Typography>
-            </Image>
-          </ImageButton>
-        </MasonryItem>
-        <MasonryItem
-          key={
-            process.env.PUBLIC_URL + "/images/sigmund-GfMkWvgn1PM-unsplash.jpg"
-          }
-        >
-          <ImageButton
-            focusRipple
-            key="Fitness Tracker"
-            href="https://azwethinkweizkd.github.io/US-Weather-Tracker-Dashboard/"
-            rel="noopener"
-          >
-            <ImageSrc
-              style={{
-                backgroundImage: `url(${
-                  process.env.PUBLIC_URL +
-                  "/images/sigmund-GfMkWvgn1PM-unsplash.jpg"
-                })`,
-              }}
-            />
-            <ImageBackdrop className="MuiImageBackdrop-root" />
-            <Image>
-              <Typography
-                component="span"
-                variant="subtitle1"
-                color="inherit"
-                sx={{
-                  position: "relative",
-                  p: 4,
-                  pt: 2,
-                  pb: (theme) => `calc(${theme.spacing(1)} + 6px)`,
-                }}
-              >
-                Weather App
-                <ImageMarked className="MuiImageMarked-root" />
-              </Typography>
-            </Image>
-          </ImageButton>
-        </MasonryItem>
-
-        <MasonryItem
-          key={
-            process.env.PUBLIC_URL +
-            "/images/david-travis-5bYxXawHOQg-unsplash.jpg"
-          }
-        >
-          <ImageButton
-            focusRipple
-            key="Note Taker"
-            href="https://express-js-note-taker-kd.herokuapp.com/"
-            rel="noopener"
-          >
-            <ImageSrc
-              style={{
-                backgroundImage: `url(${
-                  process.env.PUBLIC_URL +
-                  "/images/david-travis-5bYxXawHOQg-unsplash.jpg"
-                })`,
-              }}
-            />
-            <ImageBackdrop className="MuiImageBackdrop-root" />
-            <Image>
-              <Typography
-                component="span"
-                variant="subtitle1"
-                color="inherit"
-                sx={{
-                  position: "relative",
-                  p: 4,
-                  pt: 2,
-                  pb: (theme) => `calc(${theme.spacing(1)} + 6px)`,
-                }}
-              >
-                Note Taker
-                <ImageMarked className="MuiImageMarked-root" />
-              </Typography>
-            </Image>
-          </ImageButton>
-        </MasonryItem>
+              />
+              <ImageBackdrop className="MuiImageBackdrop-root" />
+              <Image>
+                <Typography
+                  component="span"
+                  variant="subtitle1"
+                  color="inherit"
+                  sx={{
+                    position: "relative",
+                    p: 4,
+                    pt: 2,
+                    pb: (theme) => `calc(${theme.spacing(1)} + 6px)`,
+                  }}
+                >
+                  {proj.title}
+                  <ImageMarked className="MuiImageMarked-root" />
+                </Typography>
+              </Image>
+            </ImageButton>
+          </MasonryItem>
+        ))}
       </Masonry>
     </Box>
   );
